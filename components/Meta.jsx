8bc6e62085c2cac21e8b988a93380e31ba386382
Benjamin TD <benjamin.tdm@gmail.com>
2020-10-30 11:00:42 +0100

    typo

--- components/Meta.jsx
+++ components/Meta.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Meta = () => {
   const description =
-    "Générez des attestation en une seconde, sans avoir à retaper vos infos à chaque fois.";
+    "Générez des attestations en une seconde, sans avoir à retaper vos infos à chaque fois.";
   const title = "Attestation en un clic";
   const ogImage = "https://attestation.page/og-image.png";
   return (
