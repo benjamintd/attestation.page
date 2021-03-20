710c8868016f3e10b5bc0cd90dec7e4457421c03
Benjamin TD <benjamin.tdm@gmail.com>
2021-03-20 12:56:44 +0100

    confinement au dessus

--- components/Reason.jsx
+++ components/Reason.jsx
@@ -22,41 +22,6 @@ export default function Reason({ onClick, stateValid }) {
   return (
     <div className="w-full mb-8">
       <hr />
-      <div className="py-6 font-semibold">Pour le couvre-feu, après 19h</div>
-      <button disabled={!stateValid} id="travail" onClick={onClick}>
-        <Work className="icon" />
-        Je me rends au travail ou en reviens
-      </button>
-      <button disabled={!stateValid} id="sante" onClick={onClick}>
-        <Health className="icon" />
-        Je me soigne ou vais à la pharmacie
-      </button>
-      <button disabled={!stateValid} id="famille" onClick={onClick}>
-        <Family className="icon" />
-        Je m'occupe de mes proches ou d'enfants
-      </button>
-      <button disabled={!stateValid} id="handicap" onClick={onClick}>
-        <Handicap className="icon" />
-        J'aide quelqu'un en situation de handicap
-      </button>
-      <button disabled={!stateValid} id="convocation" onClick={onClick}>
-        <Convocation className="icon" />
-        J'ai une convocation administrative
-      </button>
-      <button disabled={!stateValid} id="missions" onClick={onClick}>
-        <Mission className="icon" />
-        J'ai une mission d'intérêt général
-      </button>
-      <button disabled={!stateValid} id="transit" onClick={onClick}>
-        <Train className="icon" />
-        Je transite pour un voyage
-      </button>
-      <button disabled={!stateValid} id="animaux" onClick={onClick}>
-        <Dog className="icon" />
-        Je promène un animal de compagnie
-      </button>
-
-      <hr />
       <div className="py-6 font-semibold">
         Pour les territoires soumis au confinement, de 6h à 19h
       </div>
@@ -97,6 +62,41 @@ export default function Reason({ onClick, stateValid }) {
       <button disabled={!stateValid} id="rassemblement" onClick={onClick}>
         <People className="icon" />
         Je participe à des événements autorisés
+      </button>
+
+      <hr />
+      <div className="py-6 font-semibold">Pour le couvre-feu, après 19h</div>
+      <button disabled={!stateValid} id="travail" onClick={onClick}>
+        <Work className="icon" />
+        Je me rends au travail ou en reviens
+      </button>
+      <button disabled={!stateValid} id="sante" onClick={onClick}>
+        <Health className="icon" />
+        Je me soigne ou vais à la pharmacie
+      </button>
+      <button disabled={!stateValid} id="famille" onClick={onClick}>
+        <Family className="icon" />
+        Je m'occupe de mes proches ou d'enfants
+      </button>
+      <button disabled={!stateValid} id="handicap" onClick={onClick}>
+        <Handicap className="icon" />
+        J'aide quelqu'un en situation de handicap
+      </button>
+      <button disabled={!stateValid} id="convocation" onClick={onClick}>
+        <Convocation className="icon" />
+        J'ai une convocation administrative
+      </button>
+      <button disabled={!stateValid} id="missions" onClick={onClick}>
+        <Mission className="icon" />
+        J'ai une mission d'intérêt général
+      </button>
+      <button disabled={!stateValid} id="transit" onClick={onClick}>
+        <Train className="icon" />
+        Je transite pour un voyage
+      </button>
+      <button disabled={!stateValid} id="animaux" onClick={onClick}>
+        <Dog className="icon" />
+        Je promène un animal de compagnie
       </button>
     </div>
   );
